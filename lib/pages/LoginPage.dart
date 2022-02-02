e7d6c08a172f27f5a1738dc1cb3cf9564ba1b5c9
Ansh Doshi <anshdoshi2305@gmail.com>
2022-02-02 16:28:54 +0530

    created login page

---
+++ lib/pages/LoginPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Text(
+          "Login page",
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+          ),
+          textScaleFactor: 2.0,
+        ),
+      ),
+    );
+  }
+}
