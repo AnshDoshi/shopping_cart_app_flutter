813d15f3e4897905101d7855d7e897585c290ecf
Ansh Doshi <anshdoshi2305@gmail.com>
2022-02-02 14:40:53 +0530

    created homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/HomePage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Hiii how's you brother?"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
