e6939490125484f456fd129e73acf6a495643afb
Ansh Doshi <anshdoshi2305@gmail.com>
2022-02-02 14:43:53 +0530

    commmit

---
+++ lib/HomePage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var Name = "ansh doshi";
+
+    double pie = 3.14;
+    String name = "Ansh Doshi";
+    bool Male = true;
+    num temp = 30.0;
+
+    var day = "tuesday"; //identify variable by compliler
+    const pi = 3.14; //we cant change this value
+    final rnge = 3.11; //we can change this value
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Hii $Name how's you brother? "),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
